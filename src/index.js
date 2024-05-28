8ce62f10331a2755ce2a59553c0a5a7f63b57a2e
Shaghayegh9473 <universeplanet2010@gmail.com>
2024-05-28 23:43:49 +0800

    Added date feature and debugged the code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 function refreshWeather(response) {
   // Add feels like weather option too!
-  let cityElement = document.querySelector("weather-city");
+  let cityElement = document.querySelector("#weather-city");
   let currentTemp = document.querySelector("#current-temp");
   let iconWeather = document.querySelector("#icon");
   let weatherCondition = document.querySelector("#weather-cond");
   let weatherHumidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
   let temperature = response.data.temperature.current;
-  console.log(response.data);
+  let date = new Date(response.data.time * 1000);
 
   currentTemp.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   weatherCondition.innerHTML = response.data.condition.description;
   weatherHumidity.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
   iconWeather.innerHTML = `<img src="${response.data.condition.icon_url}" alt="Weather Icon" class = "weather-icon" />`;
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute}`;
 }
 
 function searchCity(city) {
